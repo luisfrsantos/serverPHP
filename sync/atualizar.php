6de577c4c639aaa01a432549cc02cf3617fd1023
DieguinhoHR <diego_henriquerodrigues@hotmail.com>
2016-05-07 22:23:23 -0300

    Add refactor nas classes syncs

--- sync/atualizar.php
+++ sync/atualizar.php
@@ -1,21 +1,12 @@
 <?php 
-
-
-
-
 $i = 1;
 
-
-if ($i == 1){
-	
-	$atualizacao = array('link' => 'http://192.168.0.67/manutencao11.jpg', );
-	  
-	  
-	echo  json_encode($atualizacao);
-	
-}else{
-	echo erro;
+if ($i == 1) {	
+    $atualizacao = array('link' => 'http://192.168.0.67/manutencao11.jpg', );	  
+    echo  json_encode($atualizacao);	
+} else {
+    echo erro;
 }
 
 
-?>
+
